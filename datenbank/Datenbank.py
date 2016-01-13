06e582331e73b8574b038c5adcd5b02957f66d11
project <project@daniku.de>
2016-01-13 23:42:09 +0100

    started

---
+++ datenbank/Datenbank.py
@@ -0,0 +1,16 @@
+# This file is definitions of all classes and their methods of Database
+class cls_DB:
+    """Class database is name as cls_DB"""
+    def __init__(self, fileName, fileForm):
+        """
+        :param fileName: database file name with total path
+        :param fileForm: mdb, sql, accdb, etc. it is also file extension
+        """
+        self.name = ""
+        self.fileName = fileName
+        self.file = fileForm
+
+
+if __name__ == "__main__":
+    anyDB = cls_DB('C:\tmp\sample','mdb')
+    print 'hello.'
